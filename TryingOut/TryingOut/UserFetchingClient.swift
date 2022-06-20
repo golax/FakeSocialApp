3c9551dca763050bcc24dbd3074345f62ce7240c
golax <107643818+golax@users.noreply.github.com>
2022-06-20 13:25:23 -0400

    Update UserFetchingClient.swift

--- TryingOut/TryingOut/UserFetchingClient.swift
+++ TryingOut/TryingOut/UserFetchingClient.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 struct UserFetchingClient {
-    static private let url = URL(string:"https://randomuser.me/api/?results=10&format=pretty")!
+    static private let url = URL(string:"https://randomuser.me/api/?results=10&format=pretty")! /// acts as a requester for the api, which returns a raw JSON file
     
-    static func getUsers() async throws -> String {
+    static func getUsers() async throws -> [User] {
         async let (data, _) = URLSession.shared.data(from: url)
-        return try await String(data: data, encoding: .utf8)!
+        let response = try await JSONDecoder().decode(Response.self, from: data)
+        return response.users
     }
     
 }
-
